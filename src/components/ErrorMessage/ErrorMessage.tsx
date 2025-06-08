4d79a8594616dc97ec136bd502785c26c165f6c3
Dariia K <da23v12@gmail.com>
2025-06-08 18:56:58 +0100

    assembling all components in app

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,35 +1,9 @@
 import css from "./ErrorMessage.module.css";
-import { useState } from "react";
-import SearchBar from "../SearchBar/SearchBar";
-import { fetchMovies} from "../../services/movieService";
-import type { Movie } from "../../types/movie";
-import MovieGrid from "../MovieGrid/MovieGrid";
 
+interface ErrorMessageProps {
+  message?: string;
+}
 
-export default function ErrorMessage() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isError, setIsError] = useState(false);
-
-  const handleSearch = async (query: string) => {
-    try {
-      setIsError(false);
-      const results = await fetchMovies(query);
-      setMovies(results);
-    } catch  {
-      setIsError(true);
-    }
-  };
-
-  return (
-    <>
-      <SearchBar onSubmit={handleSearch} />
-      {isError && (
-        <>
-          <p className={css.text}>There was an error, please try again...</p>
-          {movies.length > 0 && <MovieGrid movies={movies} onSelect={() => {}} />}
-
-        </>
-      )}
-    </>
-  );
+export default function ErrorMessage({ message = "There was an error, please try again..." }: ErrorMessageProps) {
+  return <p className={css.text}>{message}</p>;
 }
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
 import css from "./Loader.module.css";
-import SearchBar from "../SearchBar/SearchBar";
-import { fetchMovies } from "../../services/movieService";
-import type { Movie } from "../../types/movie";
-import MovieGrid from "../MovieGrid/MovieGrid";
 
 export default function Loader() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSearch = async (query: string) => {
-    try {
-      setIsLoading(true);
-      const results = await fetchMovies(query);
-      setMovies(results);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <>
-      <SearchBar onSubmit={handleSearch} />
-      {isLoading && <p className={css.text}>Loading movies, please wait...</p>}
-      {<MovieGrid movies={movies} onSelect={() => {}} />}
-    </>
+    <p className={css.text}>Loading movies, please wait...</p>
   );
 }
